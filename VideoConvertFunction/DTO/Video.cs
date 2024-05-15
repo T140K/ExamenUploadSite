91f6ff397be6c133b2cbe2af89c7b5166c085f9d
Darek <darek1430143@gmail.com>
2024-05-15 17:16:54 +0200

    fixed typo

--- VideoConvertFunction/DTO/Video.cs
+++ VideoConvertFunction/DTO/Video.cs
@@ -11,14 +11,16 @@ namespace VideoUploadSite.DTO
     {
         [Key]
         public int Id { get; set; }
-        public string FileName { get; set; }
-        public string BlobName { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string VideoBlobUrl { get; set; }
-        public string ProcessedVideoBlobUrl { get; set; }
-        public string ProcessingStatus { get; set; }
-        public string ThumbnailUrl { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? FileName;
+        public string? VideoBlobUrl { get; set; }
+        public string? ProcessedVideoBlobUrl { get; set; } = null;
+        public string? ThumbnailUrl { get; set; } = null;
+        public string? BlobName { get; set; } = null;
         public bool ShouldGenerateThumbnail { get; set; }
+        public string? ProcessingStatus { get; set; }
+        public string? VideoOwner { get; set; }
+        public string? VideoLink { get; set; }
     }
 }
